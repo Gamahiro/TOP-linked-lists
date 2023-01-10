017a48a44c3216820c8c327de26cf4e5b7f3000e
Johan <jpettersen50@gmail.com>
2023-01-11 00:15:49 +0100

    init

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+const LinkedList = () => {
+    function append(value) {
+
+    }
+    function prepend(value) {
+
+    }
+    function size() {
+
+    }
+    function head() {
+
+    }
+    function tail() {
+
+    }
+    function at(index) {
+
+    }
+    function pop() {
+
+    }
+    function contains(value) {
+
+    }
+    function find(value) {
+
+    }
+    function toString(value) {
+        
+    }
+}
+
+const Node = () => {
+    value = null;
+    nextNode = null
+}
